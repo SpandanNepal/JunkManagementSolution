7e64a141c4e6f5b5fc062b9e0959458d12b59f9c
Barsha Bhandari <55031294+AccioRepo@users.noreply.github.com>
2024-11-02 16:27:26 -0500

    Added header and footer (#9)

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -7,6 +7,8 @@ import VendorSignUp from './pages/VendorSignUp';
 import CustomerSignUp from './pages/CustomerSignUpPage';
 import JunkDescriptionForm from './pages/junkDescriptionForm';
 import VendorProfileForm from './pages/vendorProfileForm';
+import CustomerHomepage from './pages/CustomerHomepage';
+import VendorSearchResults from './pages/VendorSearchResults';
 
 const AppRoutes: React.FC = () => {
   return (
@@ -17,6 +19,9 @@ const AppRoutes: React.FC = () => {
       <Route path="/customersignup" element={<CustomerSignUp />} />
       <Route path="/junkdescriptionform" element={<JunkDescriptionForm />} />
       <Route path="/vendorprofileform" element={<VendorProfileForm />} />
+      <Route path="/customerhomepage" element={<CustomerHomepage />} />
+      <Route path="/vendorsearchresult" element={<VendorSearchResults />} />
+      <Route path="*" element={<Login />} />
     </Routes>
   );
 };

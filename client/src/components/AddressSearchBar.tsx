564906225f490838dc9d51e159d642f11ca56716
Barsha Bhandari <55031294+AccioRepo@users.noreply.github.com>
2024-11-09 04:57:35 -0600

    test api url (#36)

--- client/src/components/AddressSearchBar.tsx
+++ client/src/components/AddressSearchBar.tsx
@@ -1,7 +1,6 @@
 import { useState, FormEvent } from "react";
 import { FaMapMarkerAlt, FaArrowLeft, FaArrowRight } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
-import Button from "./button";
 
 export interface AddressSearchBarProps {
     onSearch: (address: string, miles: string) => void;
@@ -14,9 +13,10 @@ const AddressSearchBar: React.FC<AddressSearchBarProps> = ({ onSearch }) => {
     const [loading, setLoading] = useState<boolean>(false);
     const navigate = useNavigate();
 
+
     const handleGetStarted = () => {
         navigate(`/junkdescriptionform`);
-    };
+      };
 
     const handleAddressChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const value = e.target.value;
@@ -38,7 +38,7 @@ const AddressSearchBar: React.FC<AddressSearchBarProps> = ({ onSearch }) => {
         if (address.length === 5 && miles) {
             setLoading(true);
             try {
-                const response = await fetch(`/api/zipcodes/nearby?zipcode=${address}&distance=${miles}`);
+                const response = await fetch(`https://proximal-api-f77eafdc5b71.herokuapp.com/api/zipcodes/nearby?zipcode=${address}&distance=${miles}`);
                 const data = await response.json();
 
                 if (data.status === "success") {
@@ -87,14 +87,13 @@ const AddressSearchBar: React.FC<AddressSearchBarProps> = ({ onSearch }) => {
                         onChange={handleMilesChange}
                         className="flex-grow p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white text-black"
                     />
-                    <Button
-                        onClick={() => {}}
+                    <button
                         type="submit"
-                        className="mt-4 bg-blue-500 text-white hover:bg-blue-600 focus:ring-blue-300"
+                        className="mt-4 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300"
                         disabled={loading || address.length !== 5} // Only enable when address is 5 digits
                     >
                         {loading ? 'Loading...' : 'Submit'}
-                    </Button>
+                    </button>
                 </form>
             ) : (
                 <div className="flex flex-col items-center mt-4">
@@ -103,20 +102,20 @@ const AddressSearchBar: React.FC<AddressSearchBarProps> = ({ onSearch }) => {
                             You have {vendorCount} vendors found {miles} miles around {address}.
                         </p>
                         <div className="flex justify-between w-full">
-                            <Button
+                            <button
                                 onClick={handleGoBack}
-                                className="bg-black text-white hover:bg-orange-600 focus:ring-orange-300"
+                                className="flex items-center px-4 py-2 bg-black text-white rounded-md hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-300"
                             >
                                 <FaArrowLeft className="mr-2" />
                                 Back
-                            </Button>
-                            <Button
+                            </button>
+                            <button
                                 onClick={handleGetStarted}
-                                className="bg-blue-500 text-white hover:bg-blue-600 focus:ring-blue-300"
+                                className="flex items-center px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300"
                             >
                                 Get Started
                                 <FaArrowRight className="ml-2" />
-                            </Button>
+                            </button>
                         </div>
                     </div>
                 </div>
